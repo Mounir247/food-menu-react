58c7e85f647c502a6747a9014cdb1d155c814079
Mounir Laskri <laskri.mounir@gmail.com>
2020-04-22 13:47:14 +0200

    Add style

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,16 +7,19 @@ import styles from './CheckoutSummary.module.css';
 const checkoutSummary = (props) => {
     return (
         <div className={styles.CheckoutSummary}>
-            <h1>We hope it tastes well!</h1>
+            <h1>Enjoy this tasty Burger !</h1>
             <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
-            <Button 
+            <div className={styles.CheckoutButton}>
+                <Button 
                 btnType="Danger"
                 clicked={props.checkoutCancelled}>CANCEL</Button>
             <Button 
                 btnType="Success"
                 clicked={props.checkoutContinued}>CONTINUE</Button>
+            </div>
+            
         </div>
     );
 }
